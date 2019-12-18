a3063b4f45e62abd90d64b091f0e5ff7ffc81598
Adam Duncan <adam.duncan@clock.co.uk>
2019-12-18 19:19:11 +0000

    Add additional logging

--- index.js
+++ index.js
@@ -26,12 +26,20 @@ IkeaHome.prototype.connect = async function () {
     await this.client.connect(identity, psk)
     this.client.on('device updated', device => {
       this.devices[device.instanceId] = device
+      this.logger.info('IkeaHome: device updated:', device)
     })
     .observeDevices()
     // Every 6 hours
     setInterval(() => {
+      this.logger.info('IkeaHome: Checking battery level...')
       this.determineBatteryLevelChange()
-    }, 21600000)
+    }, this.config.batteryCheckInterval || 21600000)
+    setInterval(async () => {
+      const success = await this.client.ping()
+      if (!success) {
+        this.logger.info('IkeaHome: Pinging gateway failed!')
+      }
+    }, this.config.pingInterval || 300000)
     setTimeout(() => {
       this.determineBatteryLevelChange()
     }, 1000)
@@ -40,15 +48,18 @@ IkeaHome.prototype.connect = async function () {
   }
 }
 
-IkeaHome.prototype.setBlindPosition = function (deviceName, position, callback) {
+IkeaHome.prototype.setBlindPosition = function (deviceName, position, cb) {
   try {
     const device = this.devices[this.config.devices[deviceName]]
-    if (!device) return
+    if (!device) {
+      this.logger.error('IkeaHome: Device does not exist: ' + deviceName, position)
+      return cb()
+    }
     device.blindList[0].setPosition(100 - position)
   } catch (e) {
     this.logger.error('ERROR SETTING BLIND POSITION:', e)
   }
-  callback()
+  cb()
 }
 
 IkeaHome.prototype.determineBatteryLevelChange = async function (deviceId) {
@@ -66,6 +77,10 @@ IkeaHome.prototype.determineBatteryLevelChange = async function (deviceId) {
 
 IkeaHome.prototype.getBatteryLevel = async function (deviceName, cb) {
   const deviceId = this.config.devices[deviceName]
+  if (!deviceId) {
+    this.logger.error('IkeaHome: Device does not exist: ' + deviceName)
+    return cb()
+  }
   if (!deviceId) return cb()
   const response = await this.client.request(`15001/${deviceId}`, 'get')
   const newBatteryLevel = response.payload['3']['9']
